379f8a9f98ec3ca3f7563710a9ed28da4c16a087
Filip Zagozdon <filipzagozdon8@gmail.com>
2022-07-18 18:54:12 +0200

    Finish all 'Fundamentals: 4' exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemperature) {
+  return Math.round((fahrenheitTemperature - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusTemperature) {
+  return Math.round((celsiusTemperature * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
